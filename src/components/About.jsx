c802f2550afe7c51f1ba72c1dd69420137109b89
vjsyam <127888696+vjsyam@users.noreply.github.com>
2025-03-25 22:27:13 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
             </p>
 
             <ButtonLink
-              url='https://drive.google.com/file/d/1rg3wQQ-uhI4_E_BSv38uQWgPR6IJ0OOX/view?usp=drive_link'
+              url='https://drive.google.com/file/d/1cmQcxl3YyAAakRHwPgP9IMyAH2_Z5gwD/view?usp=drive_link'
               text='View Resume →'
               padding={`p-3`}
             />
